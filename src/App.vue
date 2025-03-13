a7d5437b34c87c1a1ad9d5133d5ea97e45d54cca
Trong Anh <nguyentronganh23022002@gmail.com>
2025-03-13 13:31:58 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <Navbar />
+    <Home />
     <router-view></router-view>
   </div>
 </template>
 
 <script setup>
 import Navbar from "./components/Header.vue";
+import Home from "./layouts/Home.vue";
 </script>
